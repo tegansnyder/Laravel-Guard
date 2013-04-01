4ce38388bf49388b581d062400df70d4236bd6c7
Jeffrey Way <jeffrey@envato.com>
2013-03-31 21:14:50 -0400

    Add stylesheet and script helpers

---
+++ src/Way/Helpers/globals.php
@@ -0,0 +1,38 @@
+<?php
+
+if ( ! function_exists('stylesheet'))
+{
+	/**
+	 * Generates the necessary HTML for a
+	 * stylesheet link. Path should be relative
+	 * to the css_path config option for Guard.
+	 *
+	 * @param  string $path
+	 * @return string
+	 */
+	function stylesheet($path = 'style.min.css')
+	{
+		$path = \Config::get('guard-laravel::guard.css_path') . "/$path";
+		$path = str_replace('public', '', $path);
+
+		return "<link rel='stylesheet' href='{$path}'>";
+	}
+}
+
+if ( ! function_exists('stylesheet'))
+	/**
+	 * Generates the necessary HTML for a
+	 * script link. Path should be relative
+	 * to the js_path config option for Guard.
+	 *
+	 * @param  string $path
+	 * @return string
+	 */
+	function script($path = 'scripts.min.css')
+	{
+		$path = \Config::get('guard-laravel::guard.js_path') . "/$path";
+		$path = str_replace('public', '', $path);
+
+		return "<script src='$path'></script>";
+	}
+}
