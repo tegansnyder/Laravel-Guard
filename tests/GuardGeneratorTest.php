c9a045923e687fd1d082f8fd73d666a735df2be5
Jeffrey Way <jeffrey@envato.com>
2013-04-01 15:06:01 -0400

    Add GuardGenerator test

--- tests/GuardGeneratorTest.php
+++ tests/GuardGeneratorTest.php
@@ -2,59 +2,51 @@
 
 use Way\Console\GuardGenerator;
 use Mockery as m;
-use Illuminate\Config\Repository as Config;
 
 class GuardGeneratorTest extends PHPUnit_Framework_TestCase {
+	public function setUp()
+	{
+		$this->file = m::mock('Illuminate\Filesystem\Filesystem');
+		$this->guardFile = m::mock('Way\Console\Guardfile');
+
+		$this->generate = new GuardGenerator($this->file, $this->guardFile);
+	}
+
 	public function tearDown()
 	{
 		Mockery::close();
 	}
 
-	public function testGuardFileCanBeCreated()
+	public function testCanCreateFolder()
 	{
-		// Mock Config
-		$configMock = m::mock('Illuminate\Config\Repository[get]');
-		$configMock->shouldReceive('get')->andReturn('path');
+		$this->file->shouldReceive('exists')->andReturn(false);
+		$this->file->shouldReceive('makeDirectory')->with('foo', 0777, true)->once();
 
-		// We don't want to REALLY create files. :)
-		$files =  m::mock('Illuminate\Filesystem\Filesystem[put]');
-
-		// We want the call to be made, though, to create the Guardfile with the contents of the stub.
-		$files->shouldReceive('put')->once()->with(__DIR__.'/Guardfile', file_get_contents(__DIR__.'/stubs/GuardFileSingleStub.rb'));
-
-		$generate = new GuardGenerator($files, $configMock);
-		$generate->guardFile(['sass'], __DIR__);
+		$this->generate->folder('foo');
 	}
 
-	public function testGuardFileCanBeCreatedWithMultiplePlugins()
+	public function testWillNotCreateFolderIfExists()
 	{
-		// Mock Config
-		$configMock = m::mock('Illuminate\Config\Repository[get]');
-		$configMock->shouldReceive('get')->andReturn('path');
+		$this->file->shouldReceive('exists')->andReturn(true);
+		$this->file->shouldReceive('makeDirectory')->never();
 
-		// We don't want to REALLY create files. :)
-		$files =  m::mock('Illuminate\Filesystem\Filesystem[put]');
-
-		$files->shouldReceive('put')->once()->with(__DIR__.'/Guardfile', file_get_contents(__DIR__.'/stubs/GuardFileMultipleStub.rb') );
-
-		$generate = new GuardGenerator($files, $configMock);
-		$generate->guardFile(['sass', 'coffeescript'], __DIR__);
+		$this->generate->folder('foo');
 	}
 
-	public function testAssetFolderCanBeCreated()
+	public function testCanCreateGuardfile()
 	{
-		// Mock Config
-		$configMock = m::mock('Illuminate\Config\Repository[get]');
-		$configMock->shouldReceive('get')->andReturn('path');
+		$this->guardFile->shouldReceive('getStubs')->once();
+		$this->guardFile->shouldReceive('put')->once();
 
-		// We don't want to REALLY create files. :)
-		$files =  m::mock('Illuminate\Filesystem\Filesystem');
-
-		// Should try to create a path/sass directory.
-		$files->shouldReceive('makeDirectory')->once()->with('path/sass', 0777, true);
-
-		$generate = new GuardGenerator($files, $configMock);
-		$generate->assetFolder('sass');
+		$this->generate->guardFile(['foo']);
 	}
 
+	public function testCanCreateLogFileForPlugins()
+	{
+		$plugins = ['foo'];
+		$this->file->shouldReceive('exists')->andReturn(true);
+		$this->file->shouldReceive('put')->with('some/path/plugins.txt', json_encode($plugins))->once();
+
+		$this->generate->log($plugins, 'some/path');
+	}
 }
